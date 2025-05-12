0984c240faa2b7bf316529e1a70e75e7d469e0ba
vrishabh19 <hrishabh031@gmail.com>
2025-05-12 16:33:12 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    esmExternals: 'loose'
-  },
   webpack: (config) => {
     config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
     return config;
